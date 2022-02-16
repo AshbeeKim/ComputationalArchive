3f2435b609cc10a83fd8c8ef43bb69df08f34974
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-02-16 23:23:10 +0900

    Update bigquery_sql.py

--- Langs/SQL/bigquery_sql.py
+++ Langs/SQL/bigquery_sql.py
@@ -23,12 +23,31 @@ class PubBigQuery:
             dataset_ref = self.client.dataset(''.join(args), project='bigquery-public-data')
             dataset = self.client.get_dataset(dataset_ref)
             return dataset_ref, dataset
-
-######################################################################################################################################
         
     def check_table(self):
-        print()
-        
+        for idx, (ref, _dataset) in enumerate(self.datasets.items()):
+            print(f"\n{str(idx+1)}. {ref}:")
+            for cnt, tables in enumerate(list(self.client.list_tables(_dataset))):
+                print(f"\t{str(cnt+1)}: {tables.table_id}\n")
+
+    def check_schema(self, path, by=None):
+        path = path.split('.')
+        table_ref = (self.dataset_refs.get(int(path[0]))).table(path[-1])
+        table = self.client.get_table(table_ref)
+        cnt = 0
+        if by is None:
+            for scm in list(table.schema):
+                print(f'Schema : {scm}')
+                cnt += 1
+        else:
+            for scm in list(table.schema):
+                if scm.field_type == by.upper():
+                    print(f'Schema : {scm}')
+                    cnt += 1
+        print(f'\nTotal Schema by "{by.upper() if by is not None else "None"}": {cnt}')
+
+######################################################################################################################################
+
     def request_table(self):
         # 
         return table_ref, table
